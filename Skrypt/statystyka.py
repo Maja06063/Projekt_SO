3240deb65dd3dfe0f3e25dd1af08c43b12e73c7f
Maja06063 <259707@student.pwr.edu.pl>
2021-12-28 17:16:41 +0100

    Klasa statystyka

---
+++ Skrypt/statystyka.py
@@ -0,0 +1,10 @@
+class Statystyka:
+
+    czas_spoznienia = []
+    czas_cyklu = []
+
+    def sredni_czas_spoznienia(self) -> int:
+        pass
+
+    def sredni_czas_cyklu(self) -> int:
+        pass
