65a1f84f55da4530d48f8d995ee1df975733f311
Maja06063 <259707@student.pwr.edu.pl>
2021-12-28 17:35:59 +0100

    Dopisanie metod

--- Skrypt/procesor.py
+++ Skrypt/procesor.py
@@ -1,4 +1,6 @@
 from zadanie import Zadanie
+from statystyka import Statystyka
+from algorytmy_szereg import AlgorytmSJF, AlgotyrmLCFS
 
 class Procesor:
     statystyka = None
@@ -7,14 +9,24 @@ class Procesor:
 
 
     def uszereguj_ciagi_zad (self):
-        pass
+        
+        self.wczytaj_zad()
+
+        for ciag in self.list_ciag_zad:
+            self.szereguj_zad(ciag)
 
     def __init__(self):
         pass
 
-    def szereguj_zad (self,ciag):
+    def szereguj_zad (self, ciag):
         pass
     
     def wczytaj_zad (self):
-        pass
-    
+        
+        plik = open("lista_ciagow.txt", "r")
+        zawartosc_pliku = plik.readlines()
+
+        for linia in zawartosc_pliku:
+            zadanie_tmczs = Zadanie()
+            [zadanie_tmczs.nr, zadanie_tmczs.t_wyk, zadanie_tmczs.t_nad] = linia.split("\t")
+            
