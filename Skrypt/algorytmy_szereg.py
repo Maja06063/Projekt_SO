bdae5c85a5b78420be3ec058d4f154af3d9370cf
Maja06063 <259707@student.pwr.edu.pl>
2021-12-28 17:12:05 +0100

    Dodanie klas Algorytm i Procesor

---
+++ Skrypt/algorytmy_szereg.py
@@ -0,0 +1,16 @@
+from zadanie import Zadanie
+
+class AlgorytmSzereg:
+
+    def wybierz_zadanie(self, dostepne_zadania):
+        raise NotImplementedError("Błąd - nie zaimplementowano metody")
+
+class AlgotyrmLCFS(AlgorytmSzereg):
+
+    def wybierz_zadanie(self, dostepne_zadania):
+        pass
+
+class AlgorytmSJF(AlgorytmSzereg):
+
+    def wybierz_zadanie(self, dostepne_zadania):
+        pass
---
+++ Skrypt/procesor.py
@@ -0,0 +1,20 @@
+from zadanie import Zadanie
+
+class Procesor:
+    statystyka = None
+    list_ciag_zad = []
+    algorytm_szeregowania = None
+
+
+    def uszereguj_ciagi_zad (self):
+        pass
+
+    def __init__(self):
+        pass
+
+    def szereguj_zad (self,ciag):
+        pass
+    
+    def wczytaj_zad (self):
+        pass
+    
