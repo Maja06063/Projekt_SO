2488990253adc9f5f1c5562b0f4aac72c9d91d88
Maja06063 <259707@student.pwr.edu.pl>
2022-01-22 17:18:36 +0100

    Początki komentarzy w kodzie

--- Skrypt/ramka.py
+++ Skrypt/ramka.py
@@ -1,13 +1,44 @@
+"""
+Klasa Ramka odpowiedzialna jest za przechowywanie stron symulowanej pamięci.
+"""
 
 class Ramka:
     rozmiar = 0
+    """
+    Metoda init zawiera sposób opisu tekstowego obiektu Ramka.
 
+    Argumenty:
+    Metoda nie przyjmuje argumentów.
+
+    Zwraca:
+    Metoda nic nie zwraca
+    """
     def __init__(self,rozmiar) -> None:
         self.rozmiar = rozmiar
         self.miejsce_w_ramce = [0]*rozmiar
+    """
+    Metoda zmien_strone  zawiera sposób opisu tekstowego obiektu Ramka.
+
+    Argumenty:
+    Metoda przyjmuje argumenty: numer_miejsca_w_ramce,strona.
+
+    Zwraca:
+    Metoda nic nie zwraca
+    """
+        
 
     def zmien_strone(self,numer_miejsca_w_ramce,strona):
         self.miejsce_w_ramce[numer_miejsca_w_ramce] = strona
+
+"""
+    Metoda init zawiera sposób opisu tekstowego obiektu Ramka.
+
+    Argumenty:
+    Metoda argument strona
+
+    Zwraca:
+    Metoda zwraca miejsce_w_ramce.
+    """
 
     def czy_zawiera(self,strona) -> bool:
         return strona in self.miejsce_w_ramce
--- Skrypt/statystyka.py
+++ Skrypt/statystyka.py
@@ -17,15 +17,63 @@ class Statystyka:
         self.czas_cyklu = [] 
         self.ilosc_podmian_stron = []
 
+    ########################################################################
+
+    """
+    Metoda sredni_czas_spoznienia wylicza średnią z zapisanych czasów spóźnienia.
+
+    Argumenty:
+    Metoda nie przyjmuje argumentów.
+
+    Zwraca:
+    Średnią arytmetyczną czasu spóźnień.
+    """
+
     def sredni_czas_spoznienia(self) -> float:
         return sum(self.czas_spoznienia) / len(self.czas_spoznienia)
+
+    ########################################################################
+
+    """
+    Metoda sredni_czas_cyklu wylicza średnią z zapisanych czasów cyklu.
+
+    Argumenty:
+    Metoda nie przyjmuje argumentów.
+
+    Zwraca:
+    Średnią arytmetyczną czasu cykli.
+    """
 
     def sredni_czas_cyklu(self) -> float:
         return sum(self.czas_cyklu) / len(self.czas_cyklu)
 
-    def __str__(self) -> str:
-        return str(self.sredni_czas_spoznienia()) + \
-            ";"  + str(self.sredni_czas_cyklu())
+    ########################################################################
+
+    """
+    Metoda srednia_ilosc_podmian_stron wylicza średnią z ilości podmian stron. 
+
+    Argumenty:
+    Metoda nie przyjmuje argumentów.
+
+    Zwraca:
+    Średnią arytmetyczną z ilości podmian stron.
+    """
 
     def srednia_ilosc_podmian_stron (self) -> float:
         return sum(self.ilosc_podmian_stron) / len(self.ilosc_podmian_stron)
+ 
+    ########################################################################
+
+    """
+    Metoda __str__ zawiera sposób opisu tekstowego obiektu Statystyka.
+
+    Argumenty:
+    Metoda nie przyjmuje argumentów.
+
+    Zwraca:
+    Tekst opisujący dany obiekt Statystyka.
+    """
+
+    def __str__(self) -> str:
+        return str(self.sredni_czas_spoznienia()) + \
+            ";"  + str(self.sredni_czas_cyklu())
